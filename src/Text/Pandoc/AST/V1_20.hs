3e28dc2f3bd4fe6e42239663f7f1dbf37b9664de
Albert Krewinkel <albert@zeitkraut.de>
2020-09-16 14:10:49 +0200

    Add definition of AST v1.20

---
+++ src/Text/Pandoc/AST/V1_20.hs
@@ -0,0 +1,16 @@
+{- |
+Module      : Text.Pandoc.AST.V1_20
+Copyright   : © 2020 Albert Krewinkel
+License     : MIT
+
+Maintainer  : Albert Krewinkel <albert@zeitkraut.de>
+Stability   : alpha
+Portability : portable
+
+Version 1.20 of the pandoc document AST.
+-}
+module Text.Pandoc.AST.V1_20
+  ( module Text.Pandoc.AST.V1_20.Definition
+  ) where
+
+import Text.Pandoc.AST.V1_20.Definition
