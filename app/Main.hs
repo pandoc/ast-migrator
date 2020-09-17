6c3e2b63dfa0b92e355592d3fb827ef3a8cee333
Albert Krewinkel <albert@zeitkraut.de>
2020-09-17 08:47:03 +0200

    Enable migrations via binary

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,28 @@
 module Main (main) where
 
-import AstMigrator (someFunc)
+import Prelude hiding (interact)
 
+import Data.Aeson (eitherDecode, encode)
+import Data.ByteString.Lazy (interact)
+import Data.List (find)
+import System.Environment (getArgs)
+import Text.Pandoc.AST.Migrator
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  let direction = case find (== "--up") args of
+        Just _ -> Up
+        Nothing -> Down
+  interact $ case direction of
+    Up   -> encode
+          . either error migrateUpToV1_21
+          . eitherDecode
+    Down -> encode
+          . either error migrateDownFromV1_21
+          . eitherDecode
+
+-- | Direction in which the migration takes place.
+data Direction
+  = Up
+  | Down
