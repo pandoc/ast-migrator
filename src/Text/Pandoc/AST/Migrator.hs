3dbe910fd768c00024bc420c29ce50772d87824c
Albert Krewinkel <albert@zeitkraut.de>
2020-09-21 17:37:07 +0200

    Migrate JSON to/from any supported format

--- src/Text/Pandoc/AST/Migrator.hs
+++ src/Text/Pandoc/AST/Migrator.hs
@@ -10,7 +10,11 @@ Portability : portable
 Migrate from or to older AST versions.
 -}
 module Text.Pandoc.AST.Migrator
-  ( migrateUpToV1_21
+  ( ASTVersion (..)
+  , migrateJSON
+  , migrateJSONWith
+    -- * migration steps
+  , migrateUpToV1_21
   , migrateUpToV1_22
   , migrateDownFromV1_21
   , migrateDownFromV1_22
@@ -20,3 +24,35 @@ import Text.Pandoc.AST.V1_20.Up (migrateUpToV1_21)
 import Text.Pandoc.AST.V1_21.Down (migrateDownFromV1_21)
 import Text.Pandoc.AST.V1_21.Up (migrateUpToV1_22)
 import Text.Pandoc.AST.V1_22.Down (migrateDownFromV1_22)
+
+import Data.Aeson (FromJSON, ToJSON, eitherDecode, encode)
+import Data.ByteString.Lazy (ByteString)
+
+data ASTVersion
+  = V1_20  -- ^ Version 1.20
+  | V1_21  -- ^ Version 1.21
+  | V1_22  -- ^ Version 1.22
+  deriving (Eq, Ord, Show)
+
+migrateJSON :: ASTVersion -- ^ initial version
+            -> ASTVersion -- ^ target version
+            -> ByteString -- ^ input JSON
+            -> ByteString -- ^ migrated JSON
+migrateJSON from to =
+  case (from, to) of
+    (V1_20, V1_20) -> id
+    (V1_20, V1_21) -> migrateJSONWith migrateUpToV1_21
+    (V1_20, V1_22) -> migrateJSONWith (migrateUpToV1_22 . migrateUpToV1_21)
+    (V1_21, V1_21) -> id
+    (V1_21, V1_22) -> migrateJSONWith (migrateUpToV1_22 . migrateUpToV1_21)
+    (V1_22, V1_22) -> id
+    (V1_22, V1_21) -> migrateJSONWith migrateDownFromV1_22
+    (V1_22, V1_20) -> migrateJSONWith (migrateDownFromV1_21 . migrateDownFromV1_22)
+    (V1_21, V1_20) -> migrateJSONWith migrateDownFromV1_21
+
+migrateJSONWith :: (FromJSON a, ToJSON b)
+                => (a -> b)    -- ^ Migration function
+                -> ByteString  -- ^ Input JSON
+                -> ByteString  -- ^ Migrated JSON
+migrateJSONWith migrator =
+  encode . either error migrator . eitherDecode
