0e94f160c86f603d758714e88528ffc61139a38f
Albert Krewinkel <albert@zeitkraut.de>
2020-09-21 16:31:52 +0200

    Add definition for V1.22

---
+++ src/Text/Pandoc/AST/V1_22.hs
@@ -0,0 +1,16 @@
+{- |
+Module      : Text.Pandoc.AST.V1_22
+Copyright   : © 2020 Albert Krewinkel
+License     : MIT
+
+Maintainer  : Albert Krewinkel <albert@zeitkraut.de>
+Stability   : alpha
+Portability : portable
+
+Version 1.22 of the pandoc document AST.
+-}
+module Text.Pandoc.AST.V1_22
+  ( module Text.Pandoc.AST.V1_22.Definition
+  ) where
+
+import Text.Pandoc.AST.V1_22.Definition
---
+++ src/Text/Pandoc/AST/V1_22/Definition.hs
@@ -0,0 +1,16 @@
+{- |
+Module      : Text.Pandoc.AST.V1_22.Definition
+Copyright   : © 2020 Albert Krewinkel
+License     : MIT
+
+Maintainer  : Albert Krewinkel <albert@zeitkraut.de>
+Stability   : alpha
+Portability : portable
+
+Version 1.22 of the pandoc document AST. This is the most recent version.
+-}
+module Text.Pandoc.AST.V1_22.Definition
+  ( module Text.Pandoc.Definition
+  ) where
+
+import Text.Pandoc.Definition
